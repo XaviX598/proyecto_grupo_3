1b89a32952674159d5db6c40049dc021ca3ffdcd
Xavier Paez <xavipaez2000@hotmail.com>
2023-09-04 12:32:25 -0500

    Noticias

---
+++ src/modules/publicaciones/helpers/noticiasCliente.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const ingresarNoticiasFachada = (bodyNoticia) =>{
+    ingresarNoticias(bodyNoticia)
+}
+export const buscarTodosFachada =async () =>{
+   return  await buscarTodos()
+}
+
+
+
+const ingresarNoticias = (bodyNoticia) =>{
+    axios.post(`http://localhost:8081/API/V1.0/Facultad/noticias`, bodyNoticia).then(r => r.data);
+    console.log('Se ha insertado una noticia')
+}
+
+const buscarTodos=async ()=>{
+  const data =  axios.get(`http://localhost:8081/API/V1.0/Facultad/noticias`).then(r => r.data);
+    return await data
+}
--- src/modules/publicaciones/pages/NoticiasPage.vue
+++ src/modules/publicaciones/pages/NoticiasPage.vue
@@ -1,13 +1,65 @@
 <template>
-  
+  <div class="tabla">
+    <table border="1">
+      <tr>
+        <th>Usuario</th>
+        <th>Fecha</th>
+        <th>Descripcion</th>
+        <th>titulo</th>
+        <th>Imagen</th>
+        <th>Video</th>
+      </tr>
+      <tr v-for="(noticia, id) in noticias" :key="id">
+        <td>{{ noticia.usuario }}</td>
+        <td>{{ noticia.fecha }}</td>
+        <td>{{ noticia.descripcion }}</td>
+        <td>{{ noticia.titulo }}</td>
+        <td>{{ noticia.imagen }}</td>
+        <td>{{ noticia.video }}</td>
+      </tr>
+    </table>
+  </div>
+
+  <div>
+          <button @click="agregarNoticia">Ingresa tu noticia</button>
+
+  </div>
+
+
 </template>
 
 <script>
-export default {
+import {
+  buscarTodosFachada,
+  ingresarNoticiasFachada,
+} from "../helpers/noticiasCliente";
 
-}
+export default {
+  data() {
+    return {
+      usuario: "Xavier",
+      noticias: [],
+    };
+  },
+
+  methods: {
+    async obtener() {
+      const data = await buscarTodosFachada();
+      this.noticias = data;
+      console.log(data);
+    },
+
+    agregarNoticia(){
+     this.$router.push({ path: "/ingresarNoticias" });
+
+    }
+  },
+
+  mounted() {
+    this.obtener();
+  },
+};
 </script>
 
 <style>
-
 </style>
