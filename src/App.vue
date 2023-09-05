a1cea8877221affaf15918046d56257331ea1fc2
Michael Garcia <msgr.2300290992@gmail.com>
2023-09-05 00:25:12 -0500

    Footer Navbar y Contactos que no apezcan en el login

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
-  <NavBarVue/>
-  <router-view/>
-  <ContactosVue/>
-  <FooterVue/>
+  <NavBarVue v-if="usuariologin!=null"/>
+  <router-view />
+  <ContactosVue v-if="usuariologin!=null"/>
+  <FooterVue v-if="usuariologin!=null"/>
 </template>
 
 <script>
-import ContactosVue from './components/Contactos.vue'
-import FooterVue from './components/Footer.vue'
-import NavBarVue from './components/NavBar.vue'
+import ContactosVue from "./components/Contactos.vue";
+import FooterVue from "./components/Footer.vue";
+import NavBarVue from "./components/NavBar.vue";
+import { mapState } from "vuex";
 
 export default {
-  components : {
+  components: {
     NavBarVue,
     ContactosVue,
-    FooterVue
-  }
-}
+    FooterVue,
+  },
+  computed: {
+    ...mapState(["usuariologin"]),
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
